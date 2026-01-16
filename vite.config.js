a76d051a520565e564f775cddc3ba30e98547451
Talha rajpoot <talharajpoot.dev@gmail.com>
2026-01-16 18:40:20 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),svgr()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy"
 });
