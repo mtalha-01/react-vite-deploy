7348a71c51094ac91ce5fa652c55acccc6daf2fa
Talha rajpoot <talharajpoot.dev@gmail.com>
2026-01-17 05:17:19 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import svgr from 'vite-plugin-svgr';
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),svgr()],
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy"
+  plugins: [react(), svgr()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
 });
